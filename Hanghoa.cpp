0323fe2af9fde42f0e177ad43e2974ed3ea0206f
lmhtung <lmhtung@gmail.com>
2024-05-26 23:15:08 +0700

    file da chinh sua

--- Hanghoa.cpp
+++ Hanghoa.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<string>
+// #include<string.h>
 using namespace std;
 
 class Hanghoa {
 private:
-    string tensp[100]; // cai nay minh dung danh sach ten cac san pham de tien viec them, xoa, sua
+    string *tensp; // cai nay minh dung danh sach ten cac san pham de tien viec them, xoa, sua
     string masp; // masp: ma san pham
     static int maso; // ma so thu tu cac loai hang vi du giay so 1 2 3 
 public:
@@ -11,20 +13,25 @@ public:
     void hienthisanpham();
     string laytensp();
     string laymasp();
+    Hanghoa();
 };
+Hanghoa::Hanghoa(){
+    tensp = new string[100];
+}
 int Hanghoa::maso = 16; 
 /*quy dinh ma so (so thu tu hang hoa) la 16
  vi minh se cho san 16 sp con lai 84 sp la minh nhap them*/ 
 void Hanghoa::themsanpham(){
-    cout << "Nhap ten hang hoa ban muon them: ";
     maso++;
-    cin >> tensp[maso];
+    cout << "Nhap ten hang hoa ban muon them: ";
+    
+    getline(cin,tensp[maso]);
     masp = "MDH" + string(4-to_string(maso).length(),'0') + to_string(maso); 
     // vi du moi khi nhap them 1 san pham thi so thu tu(maso) se tang len
     // khi do no co masp la MDH0017
 }
 string Hanghoa::laytensp(){
-    return *tensp;
+    return tensp[maso];
 }
 string Hanghoa::laymasp(){
     return masp;
@@ -36,4 +43,5 @@ int main(){
     Hanghoa a;
     a.themsanpham();
     a.hienthisanpham();
+    // cout << a.laytensp();
 }
