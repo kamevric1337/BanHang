dc212bfae1526ee549ebc92434c89278d68a5aef
nhuthinh <thinh.bdn233660@sis.hust.edu.vn>
2024-06-05 18:57:40 +0700

    them ham set,them,sua,xoa

--- HangHoa.cpp
+++ HangHoa.cpp
@@ -13,7 +13,15 @@ string Hanghoa:: lay_ten_san_pham(){
 string Hanghoa::lay_ma_don_hang(){
     return ma_san_pham;
  }
-
+void Hanghoa::setTensp(string tensp){
+    san_pham=tensp;
+}
+void Hanghoa::setMasp(string ma){
+    ma_san_pham=ma;
+}
+void Hanghoa::setGiasp(double gia){
+    gia_thanh=gia;
+}
 void Hanghoa::nhap(){
     cout << "Nhap thong tin san pham: ";
     cout << "\nNhap ten san pham: ";
@@ -25,4 +33,37 @@ void Hanghoa::nhap(){
 }
 void Hanghoa::xuat(){
     cout << setw(34) << san_pham << setw(34) << ma_san_pham << gia_thanh << endl;
+}
+void them_san_pham( vector<Hanghoa>& san_pham){
+    Hanghoa sp;
+    sp.nhap();
+    san_pham.push_back(sp);
+}
+void Hanghoa::sua_san_pham(vector<Hanghoa>& san_pham){
+    string masp;
+    cout << "nhap ma so cua san pham can sua:";
+    getline(cin,masp);
+    for(auto& hanghoa:san_pham){
+        if(hanghoa.lay_ma_don_hang()==masp){
+        string ten;
+        string ma;
+        double gia;
+         cout << "Nhap thong tin san pham moi: ";
+        cout << "\nNhap ten san pham moi: ";
+        getline(cin,ten);
+        cout << "\nNhap ma san pham moi: ";
+        getline(cin,ma);
+        cout << "\nNhap gia thanh san pham moi: ";
+        cin >> gia;
+        hanghoa.setTensp(ten);
+        hanghoa.setMasp(ma);
+        hanghoa.setGiasp(gia);
+        }
+    }
+void xoa_san_pham(vector<Hanghoa>& san_pham){
+    string ma_sp_xoa;
+    cout << "Nhap ma san pham can xoa: ";
+    getline(cin,ma_sp_xoa);
+    
+}
 }
